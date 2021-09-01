7602ccb6480ced6d25220f4cd62fed2bcef24894
yuanfang000205 <2296787770@qq.com>
2021-09-01 15:10:38 +0800

    Spring注解式开发

---
+++ spring-day6/src/main/java/com/yuanfang/action/UserAction.java
@@ -0,0 +1,38 @@
+package com.yuanfang.action;
+
+import com.yuanfang.entity.User;
+import com.yuanfang.service.UserService;
+import com.opensymphony.xwork2.Action;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller("userAction")
+@Scope("prototype")
+public class UserAction {
+
+    @Autowired
+    private UserService userService;
+
+    private List<User> users;
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * 查询所有
+     * @return
+     */
+    public String findAll(){
+        this.users = userService.findAll();
+        return Action.SUCCESS;
+    }
+
+}
